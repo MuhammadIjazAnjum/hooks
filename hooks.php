bed48246d6f209a9121585d51ac41fd2b3a7087e
MuhammadIjazAnjum <id4ijaz@gmail.com>
2017-04-03 09:12:46 -0700

    calling function
    three ways to use the function in add filter

--- hooks.php
+++ hooks.php
@@ -4,3 +4,16 @@ Plugin Name:Hooks
 Description:Learning use of hooks in wordpress
 Author:Muhammad Ijaz Anjum
 */
+//one approach to call a function
+
+// add_filter('the_content',function ($content){
+// 	return $content."some Returned document texts";
+// });
+ 
+// add_filter('the_content','content_modification_fun');
+//  function content_modification_fun($content){
+	
+// 	return $content."some additional text";
+// }
+//
+// add_filter('the_content',ucwords);
